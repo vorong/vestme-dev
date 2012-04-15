139b5110d8445842c8916df72b97b0e029b0dfe5
Gary Voronel <gvo@Garys-MacBook-Air.local>
2012-04-14 22:47:38 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gvodev::Application.config.secret_token = '2aec23e46220cbfbbd0261acf7366051c63b04f8454613540a38b72f0bb52d387ee1dccdb17b739b37b2e5605075fad604d8465cf844d9690fa181b5b4fbe84a'
