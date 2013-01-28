56c9bd94663caf873348996a291a9c3f4199a160
Gary Voronel <gary@voronel.com>
2013-01-28 08:41:13 -0800

    finish user signup without any styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ActiveRecord::Base
-	validates :email, :uniqueness => true
+	attr_accessible :email
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@microsoft\.com\z/i
+	
+	before_save { |user| user.email = email.downcase }
+
+	validates :email, presence: true,
+					uniqueness: { case_sensitive: false },
+					format: { with: VALID_EMAIL_REGEX }
 end
