dee6711bb65d1537df21f59be29490b00e5fda0f
Gary Voronel <gary@voronel.com>
2013-01-29 00:02:37 -0800

    remove unecessary methods from user, route error messages back to homepage, route success to new page, remove unneeded views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,42 +1,4 @@
 class UsersController < ApplicationController
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @users }
-    end
-  end
-
-  # GET /users/1
-  # GET /users/1.json
-  def show
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user }
-    end
-  end
-
-  # GET /users/new
-  # GET /users/new.json
-  def new
-    @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
-  end
-
-  # GET /users/1/edit
-  def edit
-    @user = User.find(params[:id])
-  end
-
   # POST /users
   # POST /users.json
   def create
@@ -47,40 +9,18 @@ class UsersController < ApplicationController
         # Tell the UserMailer to send a welcome Email after save
         UserMailer.welcome_email(@user).deliver
 
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { render action: "success" }
         format.json { render json: @user, status: :created, location: @user }
       else
-        format.html { render action: "new" }
+        error_code = "SIGNUP_ERROR_UNKNOWN"
+        case @user.errors.full_messages[0]
+        when "Email can't be blank" then error_code = "SIGNUP_ERROR_EMAIL_EMPTY"
+        when "Email is invalid" then error_code = "SIGNUP_ERROR_EMAIL_INVALID"
+        when "Email has already been taken" then error_code = "SIGNUP_ERROR_EMAIL_DUPLICATE"
+        end
+        format.html { redirect_to root_url + "?email=" + @user.email, notice: error_code }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PUT /users/1
-  # PUT /users/1.json
-  def update
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
     end
   end
 end
