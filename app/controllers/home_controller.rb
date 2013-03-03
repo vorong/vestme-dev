6d867278e622be16b4e25d943f92714a2c661340
Gary Voronel <gary@voronel.com>
2013-03-03 15:26:32 -0800

    remove all auth temporarily

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  http_basic_authenticate_with :name => "mennen", :password => "h4x0rPW!"
+  #http_basic_authenticate_with :name => "mennen", :password => "h4x0rPW!"
 
   def index
   end
