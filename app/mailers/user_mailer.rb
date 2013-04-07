aafd1fefc86ce8b7d1380a7a84ac2db011035ac4
Gary Voronel <gary@voronel.com>
2013-04-06 20:01:44 -0700

    update verbage; remove image from poster; remove carousel for charts; fix bug on Android?; fix rendering on WP8 so like WP7; change to MSFT401k instead of msft401k

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "The msft401k Team <team@msft401k.com>"
+  default from: "The MSFT401k Team <team@MSFT401k.com>"
 
   def welcome_email(user)
     @user = user
-    @url  = "http://www.msft401k.com"
-    mail(:to => user.email, :subject => "Welcome to msft401k!")
+    @url  = "http://www.MSFT401k.com"
+    mail(:to => user.email, :subject => "Welcome to MSFT401k!")
   end
 end
