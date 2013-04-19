0b2abc083091dcc3c3ce36c9825bc84af8df4161
Gary Voronel <gary@voronel.com>
2013-04-19 11:01:33 -0700

    remove password from site thus making it live to the world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  http_basic_authenticate_with :name => "fafmode", :password => "pw123go"
+  #http_basic_authenticate_with :name => "fafmode", :password => "pw123go"
 end
