5087463186327089f3ff9143737628dbd2d12d38
Gary Voronel <gary@voronel.com>
2013-04-22 21:32:29 -0700

    udpate daily report with user status types

--- db/migrate/20130423033745_add_status_to_users.rb
+++ db/migrate/20130423033745_add_status_to_users.rb
@@ -1,15 +1,15 @@
 # Adding status to track active, unsubscribed, test users, etc.  Definitions below.
 # 
-# =======================================================
+# ------+----------------------------------------------
 # Value	| Meaning
-# =======================================================
+# ------+----------------------------------------------
 #     0	| Active (default)
 #     1	| Unsubscribed
 #     2	| Invalid email address, e.g. got email bounce
 #     3	| Beta test account, e.g. friends and family
 #     4	| Dev account, e.g. one of us
 #    5+	| Reserved for future use
-# =======================================================
+# ------+----------------------------------------------
 #
 
 class AddStatusToUsers < ActiveRecord::Migration
